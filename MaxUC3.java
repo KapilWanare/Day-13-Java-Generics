a045e3ae455f5b4694a4c01cb34f62b1c50a8a90
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-03-21 16:49:33 +0530

    This Branch Shows The Maximum Integer , Float , String Values

---
+++ MaxUC3.java
@@ -0,0 +1,26 @@
+public class MaxUC3 {
+    //string
+    public void FindMaxString(String i,String h,String g){
+        String max=i;
+        if((h.compareTo(max))>0)
+        {
+            max=h;
+        }
+        if((g.compareTo(max))>0){
+            max=g;
+        }
+        System.out.println("Maximum of "+i+ ", "+h+" and "+g+ " is: "+max);
+    }
+
+    public static void main(String[] args) {
+        MaxUC3 max = new MaxUC3();
+        String g="amit",h="bunny",i="carl";
+        System.out.println("********  String values  ********");
+        // test case 1 :- Given Max Number at 1st Position return the Same Number
+        max.FindMaxString(i,h,g);
+        // test case 2 :- Given Max Number at 2nd Position return the Same Number
+        max.FindMaxString(h,i,g);
+        // test case 3 :- Given Max Number at 3rd Position return the Same Number
+        max.FindMaxString(g,h,i);
+    }
+}
